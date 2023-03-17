8c265892a227af95ab067f90317c7dde4cd83d82
PaulinaCarcamo <108833481+PaulinaCarcamo@users.noreply.github.com>
2023-03-16 22:38:06 -0300

    Getting ready to deploy site

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import store from './redux/store';
 import { Provider } from 'react-redux';
+import store from './redux/store';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './sass/theme.scss';
 
